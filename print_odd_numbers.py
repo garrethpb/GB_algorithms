d05e5010abdbb02064a1d7918c5d56e8999d66a8
Garreth Broesche <gbroesche@gmail.com>
2017-10-05 10:42:02 -0500

    added a few more algorithms

---
+++ print_odd_numbers.py
@@ -0,0 +1,3 @@
+# print odd numbers from 1 to 1000, using for loop
+for print_numbers in range(1, 1001, 2):
+	print print_numbers
---
+++ sum_numbers_in_list.py
@@ -0,0 +1,6 @@
+# sum all numbers in list a = [1, 2, 5, 10, 255, 3]
+list_a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in list_a:
+	sum += i
+print sum
